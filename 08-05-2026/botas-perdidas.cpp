96bfb8f96e8f062862e5cb0f3c703eb24af6391e
Icaro Gabriel <164355795+IcaroGabrielIcaro@users.noreply.github.com>
2026-05-07 11:39:39 -0300

    refactor: botas-perdidas.cpp para uso de set

--- 08-05-2026/botas-perdidas.cpp
+++ 08-05-2026/botas-perdidas.cpp
@@ -1,33 +1,62 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-int main() {
+struct Sapato {
+    int tamanho;
+    int esquerdo;
+    int direito;
+
+    bool operator<(const Sapato& other) const {
+        return tamanho < other.tamanho;
+    }
+};
+
+int main(){
 
     int N;
-    while (cin >> N) {
+    cin >> N;
 
-        vector<int> esquerdo;
-        vector<int> direito;
-        for (int i = 0; i < N; i++) {
-            int M;
-            char L;
+    set<Sapato> sapatos;
+    int resultado = 0;
+    for (int i = 0; i < N; i++) {
+        char C;
+        int L;
 
-            cin >> M >> L;
+        cin >> L >> C;
 
-            if (L == 'E') {
-                esquerdo.push_back(M);
+        auto encontrado = sapatos.lower_bound({L, 0, 0});
+    
+        if (encontrado != sapatos.end() && encontrado->tamanho == L) {
+            auto& temporario = encontrado;
+            sapatos.erase(encontrado);
+
+            if (C == 'E') {
+                sapatos.insert({temporario->tamanho, temporario->esquerdo + 1, temporario->direito});
             } else {
-                direito.push_back(M);
+                sapatos.insert({temporario->tamanho, temporario->esquerdo, temporario->direito + 1});
+            }
+
+            auto encontrado2 = sapatos.lower_bound({L, 0, 0});
+
+            if (encontrado2 != sapatos.end()) {
+                if (encontrado2->esquerdo > 0 && encontrado2->direito > 0) {
+                    if (encontrado2->esquerdo > encontrado2->direito) {
+                        resultado
+                    } else {
+                        
+                    }
+                }
+            }
+            
+        } else {
+            if (C == 'E') {
+                sapatos.insert({L, 1, 0});
+            } else {
+                sapatos.insert({L, 0, 1});
             }
         }
-
-        sort(esquerdo.begin(), esquerdo.end());
-        sort(direito.begin(), direito.end());
-
-        // for ()
+    
     }
-
-    return 0;
-
+    
 }
