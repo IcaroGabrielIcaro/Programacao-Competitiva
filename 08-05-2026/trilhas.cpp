68321bd6e45bd9340d823767e8feff933722012d
Icaro Gabriel <icarogabrielpc05@gmail.com>
2026-05-07 21:31:46 -0300

    feat: polen e trilhas incompletos

---
+++ 08-05-2026/trilhas.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        int M;
+        cin >> M;
+
+        for (int j = 0; j < M; j++) {
+            int H;
+            cin >> H;
+        }
+    }
+
+    return 0;
+
+}
