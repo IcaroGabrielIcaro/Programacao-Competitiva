214f292bf005064c4febc847841130f8c7cd78ae
Icaro Gabriel <icarogabrielpc05@gmail.com>
2026-06-27 08:17:58 -0300

    feat: chuva obi 2019

---
+++ aulas/26-06-2026/chuva.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+vector<string> mp(500);
+queue<pair<int, int>> q;
+
+void bfs(pair<int, int> pos) {
+    if (pos.first < 0 || pos.first >= N || pos.second < 0 || pos.second >= M) {
+        return;
+    }
+
+    if (mp[pos.first][pos.second] == '#') {
+        return;
+    }
+
+    mp[pos.first][pos.second] = 'o';
+
+    if (pos.first + 1 < N && mp[pos.first + 1][pos.second] == '#') {
+        if (mp[pos.first][pos.second - 1] != 'o') {
+            q.push({pos.first, pos.second - 1});
+        }
+
+        if (mp[pos.first][pos.second + 1] != 'o') {
+            q.push({pos.first, pos.second + 1});
+        }
+    } else {
+        q.push({pos.first + 1, pos.second});
+    }
+
+    while (!q.empty()) {
+        auto a = q.front();
+        q.pop();
+        bfs(a);
+    }
+}
+
+int main() {
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        cin >> mp[i];
+    }
+
+    for (int i = 0; i < M; i++) {
+        if (mp[0][i] == 'o') {
+            q.push({0, i});
+        }
+    }
+
+    pair<int, int> a = q.front();
+    q.pop();
+    bfs(a);
+
+    for (int i = 0; i < N; i++) {
+        cout << mp[i] << endl;
+    }
+
+    return 0;
+}
