9cb175cf3927668e9a56ee2cdc95318f553a400e
IcaroGabrielIcaro <icarogabrielpc05@gmail.com>
2026-07-25 11:00:30 -0300

    refactor> p n q

--- atcoder/abc-468/p-n-q.cpp
+++ atcoder/abc-468/p-n-q.cpp
@@ -3,18 +3,48 @@
 using namespace std;
 
 int N;
-vector<int> Ps(20);
-vector<int> Qs(20);
+vector<int> Ps(10);
+vector<int> Qs(10);
 
-vector<bool> disp(20, 1);
+vector<bool> disp(11, 1);
 vector<int> atual;
+
+int ans = 0;
 void dfs(int pos) {
-    if (pos == N) return;
+    if (pos == N) {
+
+        bool greaterThanP = false;
+        for(int i=0; i<N; i++){
+            if(atual[i] > Ps[i]) {
+                greaterThanP = true;
+                break;
+            } else if(atual[i] < Ps[i]){
+                greaterThanP=false;
+                break;
+            }
+        }
+        
+        bool lessThanQ = false;
+        for(int i=0; i<N; i++){
+            if(atual[i] < Qs[i]){
+                lessThanQ = true;
+                break;
+            } else if(atual[i] > Qs[i]){
+                lessThanQ = false;
+                break;
+            }
+        }
+
+        if(lessThanQ && greaterThanP) ans++;
+
+        return;
+    } 
+        
 
     for (int i = 1; i <= N; i++) {
         if (disp[i]) {
             disp[i] = 0;
-            atual.push_back(i);
+            atual.push_back(i); 
 
             dfs(pos + 1);
 
@@ -29,10 +59,11 @@ int main() {
     cin >> N;
 
     for (int i = 0; i < N; i++) cin >> Ps[i];
-
     for (int i = 0; i < N; i++) cin >> Qs[i];
 
     dfs(0);
+
+    cout << ans;
 
     return 0;
     
